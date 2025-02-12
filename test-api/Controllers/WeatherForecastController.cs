70a6a8d4d4d40b7b22bd8eb92a11d214f9c6d1d1
dsmit089 <58849023+dsmit089@users.noreply.github.com>
2025-02-12 11:36:23 +0530

    add crash API

--- test-api/Controllers/WeatherForecastController.cs
+++ test-api/Controllers/WeatherForecastController.cs
@@ -21,6 +21,22 @@ namespace test_api.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+
+            
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "GetCrash")]
+        public IEnumerable<WeatherForecast> GetCrash()
+        {
+
+            Environment.Exit(1);
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
